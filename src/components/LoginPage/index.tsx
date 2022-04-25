ad06ec156206c376bd1b68611c0cf83046d1c0ae
areamluersen <areamluersen@gmail.com>
2022-04-25 20:45:04 -0300

    feat: add basic router structure

---
+++ src/components/LoginPage/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const LoginPage: React.FC = () => {
+  return     (
+    <div>
+        <header>
+            <p>Hello Vite + React! + Tailwind</p>
+            <p>
+            TODO - login Page Tripsy
+            </p>
+        </header>
+    </div>);
+}
+
+export default LoginPage;
---
+++ src/components/Table.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import LoginPageButton from './LoginPageButton'
+
+const Table: React.FC = () => {
+
+  return <table className="table-auto text-2xl">
+  <thead>
+    <tr>
+      <th className="rounded-l-2xl bg-slate-500 px-4">Page</th>
+      <th className='bg-slate-500 mx-2'>Description</th>
+      <th className='rounded-r-2xl bg-slate-500 px-4'>Access</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr className='border-b'>
+      <td>Login</td>
+      <td className='px-10'>Recreate Login Page of Tripsy</td>
+      <LoginPageButton />
+    </tr>
+    <tr className='border-b'>
+      <td>To-do</td>
+      <td>Create a To-do List</td>
+      <td>Soon</td>
+    </tr>
+  </tbody>
+</table>;
+}
+
+export default Table;
