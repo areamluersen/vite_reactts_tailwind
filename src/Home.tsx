fd07853582fe46614b59b937c9f4cab4cfcddbd3
areamluersen <areamluersen@gmail.com>
2022-04-25 21:32:13 -0300

    feat: add card user

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,24 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import List from './components/Table'
+
+
+function CardUser() {
+  return (
+    <div className='bg-gradient-to-r from-slate-500 to-slate-800 p-2 mb-20 rounded-lg grid grid-cols-3'>
+      <div className='flex justify-start w-40'>
+        <img src={'https://avatars.githubusercontent.com/u/24281509?v=4'} className="rounded-full" alt="logo" />
+      </div>
+      <div className='pl-6 col-span-2 flex justify-start text-3xl'>
+        <div className='grid align-middle content-center justify-baseline'>
+          <p > Aream Luersen</p>
+          <p className='text-2xl'>Full Stack Developer</p>  
+          <a href='https://github.com/areamluersen' className='text-blue-500 text-2xl'>Github</a>  
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function Home() {
   const [count, setCount] = useState(0)
@@ -9,7 +26,7 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <CardUser />
         <h2>Hello Vite + React! + Tailwind</h2>
         <h2>Some examples</h2>
            <List />
--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import {
   Route
 } from "react-router-dom";
 import LoginPage from './components/LoginPage'
-import Home from './App';
+import Home from './Home';
 
 function Places() {
   return (
