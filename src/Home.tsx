98021042da0b8c2be87d9b4087184edc48fbd4ca
Aream Luersen <areamluersen@gmail.com>
2022-05-25 22:35:31 -0300

    feat: add background image for project

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { CardUser } from './components/CardUser';
 import Header from './components/Header';
 import List from './components/Table';
+import { Footer } from './Footer';
 
 function Home() {
   return (
@@ -12,36 +13,7 @@ function Home() {
         <h2>Some dev Examples</h2>
         <List />
       </body>
-      <footer>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind
-          </a>
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
